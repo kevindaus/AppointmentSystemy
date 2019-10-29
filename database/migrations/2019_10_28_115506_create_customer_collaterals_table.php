01918da943b52501964ea5f68714fbbd1a8039e9
Kevin Florenz Daus <kevinflorenzdaus@gmail.com>
2019-10-29 20:08:50 +0800

    FIX - typo on class name

--- database/migrations/2019_10_28_115506_create_customer_collaterals_table.php
+++ database/migrations/2019_10_28_115506_create_customer_collaterals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCustomerCollaterlas extends Migration
+class CreateCustomerCollateralsTable extends Migration
 {
     /**
      * Run the migrations.
