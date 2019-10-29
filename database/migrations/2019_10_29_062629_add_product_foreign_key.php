89c1380c1d30b20faaa49b2d6a91f97b1962f854
Kevin Florenz Daus <kevinflorenzdaus@gmail.com>
2019-10-29 20:24:58 +0800

    UPDATE - migration code for adding foreign key on products column at credit_application table to products table

--- database/migrations/2019_10_29_062629_add_product_foreign_key.php
+++ database/migrations/2019_10_29_062629_add_product_foreign_key.php
@@ -13,7 +13,9 @@ class AddProductForeignKey extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('credit_applications', function (Blueprint $table) {
+            $table->foreign('product')->on('products')->references('id')->onDelete('set null');
+        });
     }
 
     /**
