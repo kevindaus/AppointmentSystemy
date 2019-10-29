bd3ba4805151bc0d3a281ed1db7b2eef1191ab60
Kevin Florenz Daus <kevinflorenzdaus@gmail.com>
2019-10-29 20:13:05 +0800

    UPDATE - renamed class name because of typo UPDATE -  migration code for sales id column

--- database/migrations/2019_10_28_115550_create_payment_history_table.php
+++ database/migrations/2019_10_28_115550_create_payment_history_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePaymentHistory extends Migration
+class CreatePaymentHistoryTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,8 +15,10 @@ class CreatePaymentHistory extends Migration
     {
         Schema::create('payment_history', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger("sale_id")->nullable();
             $table->timestamps();
         });
+
     }
 
     /**
