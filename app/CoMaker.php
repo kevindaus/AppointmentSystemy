8c5e059dd787de0147b64520abd5dce55097e7a1
Kevin Florenz Daus <kevinflorenzdaus@gmail.com>
2019-11-10 11:15:43 +0800

    UPDATE - credit application co makers relationship

--- app/CoMaker.php
+++ app/CoMaker.php
@@ -7,13 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class CoMaker extends Model
 {
     protected $guarded = [];
-    public function credit_application()
-    {
-        return $this->belongsTo(CreditApplication::class, "co_maker");
-    }
 
     public function getFullName()
     {
         return sprintf("%s %s %s %s", $this->title, $this->first_name, $this->middle_name, $this->last_name);
+    }
+    public function credit_application_co_maker()
+    {
+        return $this->hasMany(CreditApplication::class);
     }
 }
