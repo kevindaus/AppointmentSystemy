646260ae38365be88941885f4aae8239c0d77bea
Kevin Florenz Daus <kevinflorenzdaus@gmail.com>
2019-11-10 11:15:14 +0800

    ADD - new pivot table and pivot model
    ADD - Staff model

--- app/Staff.php
+++ app/Staff.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Staff extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function getFullName()
+    {
+        return sprintf("%s. %s %s %s %s", $this->title, $this->first_name, $this->middle_name, $this->last_name, $this->suffix);
+    }
+
 }
