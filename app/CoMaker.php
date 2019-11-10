62d63a0fe485605062b83c595ca6e97ed46537b5
Kevin Florenz Daus <kevinflorenzdaus@gmail.com>
2019-11-10 09:54:58 +0800

    UPDATE - relationship tables

--- app/CoMaker.php
+++ app/CoMaker.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class CoMaker extends Model
 {
-    //
+    protected $guarded = [];
+    public function credit_application()
+    {
+        return $this->belongsTo(CreditApplication::class, "co_maker");
+    }
+
+    public function getFullName()
+    {
+        return sprintf("%s %s %s %s", $this->title, $this->first_name, $this->middle_name, $this->last_name);
+    }
 }
