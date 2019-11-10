8c3d5f03932f8289b155df1035ccae3804aeff42
Kevin Florenz Daus <kevinflorenzdaus@gmail.com>
2019-11-10 09:53:33 +0800

    ADD - utility function

---
+++ app/Staffs.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Staffs extends Model
+{
+    protected $guarded = [];
+
+    public function getFullName()
+    {
+        return sprintf("%s. %s %s %s %s", $this->title, $this->first_name, $this->middle_name, $this->last_name, $this->suffix);
+    }
+
+}
