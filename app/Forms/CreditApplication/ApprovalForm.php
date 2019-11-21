b63c0be0007ffb5129c536bac614079cf3b17949
Kevin Florenz Daus <kevinflorenzdaus@gmail.com>
2019-11-21 15:19:40 +0800

    FIX - ui issue

---
+++ app/Forms/CreditApplication/ApprovalForm.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Forms\CreditApplication;
+
+
+class ApprovalForm
+{
+
+}
--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,29 +2,83 @@
 
 namespace App\Http\Controllers;
 
-use App\CreditApplication;
-use App\Notifier\DuePaymentSMSNotifier;
 use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
-    public function due_payment()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
-        /* @TODO - show list of unpaid dues*/
-        $overDuecreditApplications = CreditApplication::all();// get all overdue
-        return view('payment.over_due')->with(compact('overDuecreditApplications'));
+        //
     }
 
-    public function send_due_notification(DuePaymentSMSNotifier $duePaymentSMSNotifier)
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
     {
-        $username_api = setting('API_USERNAME');
-        $password_api = setting('API_PASSWORD');
-        $duePaymentSMSNotifier->setCredential($username_api, $password_api);
+        //
+    }
 
-        /* @TODO - get all overdued payment */
-        /* foreach  customer  , get their mobile number*/
-        /* prepare message  */
-        /* send message */
-        $duePaymentSMSNotifier->send();
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
