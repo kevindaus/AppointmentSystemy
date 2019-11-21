5fb8346ebc03f006181261b35279325b654872d7
Kevin Florenz Daus <kevinflorenzdaus@gmail.com>
2019-11-21 15:21:18 +0800

    ADD - specify date field

--- app/CreditApplication.php
+++ app/CreditApplication.php
@@ -3,11 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class CreditApplication extends Model
 {
+
     protected $guarded = [];
-    protected $dates = ['due_date', 'request_due_date'];
+    protected $dates = ['due_date', 'request_due_date','date_received_by_ci'];
     const STATUS_PENDING = 'pending';
     const STATUS_APPROVED = 'approved';
     const STATUS_DENIED = 'denied';
@@ -37,5 +39,58 @@ class CreditApplication extends Model
         return $this->hasMany(CreditApplicationCoMaker::class);
     }
 
+    public function product()
+    {
+        $product = DB::table('credit_application_product')->where(['credit_application_id' => $this->id])->first();
+        return Product::findOrFail($product->product_id);
+    }
 
+    public function co_maker()
+    {
+        $coMaker = DB::table('credit_application_co_maker')->where(['credit_application_id' => $this->id])->first();
+        return CoMaker::findOrFail($coMaker->co_maker_id);
+    }
+
+    public function getFieldSalesman()
+    {
+        $creditAppStaff = DB::table('credit_application_staff')->where(
+            [
+                'credit_application_id' => $this->id,
+                'role' => Staff::POSITION_FIELD_SALESMAN,
+            ]
+        )->first();
+        return Staff::findOrFail($creditAppStaff->staff_id);
+    }
+
+    public function getCIReceiver()
+    {
+        $creditAppStaff = DB::table('credit_application_staff')->where(
+            [
+                'credit_application_id' => $this->id,
+                'role' => Staff::POSITION_CREDIT_RECEIVER,
+            ]
+        )->first();
+        return Staff::findOrFail($creditAppStaff->staff_id);
+    }
+
+    public function getCorrectAndConfirm()
+    {
+        $creditAppStaff = DB::table('credit_application_staff')->where(
+            [
+                'credit_application_id' => $this->id,
+                'role' => Staff::POSITION_CLARIFIER,
+            ]
+        )->first();
+        return Staff::findOrFail($creditAppStaff->staff_id);
+    }
+    public function getBranchManager()
+    {
+        $creditAppStaff = DB::table('credit_application_staff')->where(
+            [
+                'credit_application_id' => $this->id,
+                'role' => Staff::POSITION_BRANCH_MANAGER,
+            ]
+        )->first();
+        return Staff::findOrFail($creditAppStaff->staff_id);
+    }
 }
